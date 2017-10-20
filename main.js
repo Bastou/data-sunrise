2c118af499293f6b3307912f245dddc929bd2578
Bastien CORNIER <bastien.cornier@gmail.com>
2017-10-20 07:51:56 +0200

    add sun + clip canyon + oopti

--- main.js
+++ main.js
@@ -1,215 +1,355 @@
+// audio context 
+window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
 
- // audio context 
- window.AudioContext=window.AudioContext||window.webkitAudioContext||window.mozAudioContext;
 
+var audioFile = 'lorn-acid-rain.mp3';
 
- var audioFile = 'lorn-acid-rain.mp3';
- /**
-  *
-  * Constantes
-  *
-  */  
-  var EASING = 0.1
-  var LINELENGTH = 20;
-  var TRANSLATESTEP = 1 ;
+var frequences = new Float32Array(256); // TODO: a mettre dans audio manager
+/**
+ *
+ * Constantes
+ *
+ */
+var EASING = 0.1
+var LINELENGTH = 20;
+var TRANSLATESTEP = 1;
+
+/*
+ * Time stuff
+ */
+var DELTA_TIME = 0;
+var LAST_TIME = Date.now();
+
+/*
+ * Canvas stuff
+ */
+
+var W = innerWidth;
+var H = innerHeight;
+var frameIndex = 0;
+var lineBeginOffset = -40;
+
+/*
+ * Test
+ */
+ var EASING = 0.3;
+ var noise = 0;
+ // Sun
+var sunEasingValue;
+// Light
+var lightEasingValue;
+var iterationCount = 0;
+var totalIterations = 2 * 60; // 25seconds 
+
+function Scene() {
+  this.cv;
+  this.ctx;
+  this.ocv;
+  this.octx;
+  this.cvS;
+  this.freqLine;
+  this.canyon;
+  this.sun;
 
   /*
-   * Time stuff
+   * Sound
    */
-   var DELTA_TIME = 0;
-   var LAST_TIME = Date.now();
+  this.audioCtx;
+  this.audioBuffer;
+  this.audioSource;
+  this.analyser;
+  this.frequencyData;
+  this.frequences;
+}
 
-   /*
-   * Canvas stuff
-   */
+Scene.prototype = {
+  init: function() {
+    this.cv = document.querySelector('canvas');
+    this.ctx = this.cv.getContext('2d');
+    this.ocv = document.createElement('canvas');
+    this.octx = this.ocv.getContext('2d');
+    this.onResize();
 
-   var W = innerWidth;
-   var H = innerHeight;
-   var frameIndex = 0;
+    // init frequency line
+    this.freqLine = new FreqLine({
+      w: W/2,
+      h: H/4,
+      lineWidth: 4,
+      startOffset:-40,
+      easeFactor:0.1
+    });
 
+    // init canyon
+    this.canyon = new Canyon({
+      x: 0,
+      y: 0,
+      octx: this.octx,
+      ocv: this.ocv,
+      zoom: 1.02,
+      canyonLine: this.freqLine
+    });
 
+    // init sun
+    this.sun = new Sun({
+      x: W/2,
+      y: H - H/3 - 20,
+      r: W/10,
+      nbPoints:60
+    });
+    this.sun.setPoints();
 
+    this.loadSound();
+  },
+  loadSound: function(url) {
+    var self = this;
+    var request = new XMLHttpRequest();
+    request.open('GET', audioFile, true);
+    request.responseType = 'arraybuffer';
 
-   function SoundReact () {
-     this.cv;
-     this.ctx;
-     this.ocv;
-     this.octx;
-     this.cvS;
-     this.linesArray = [];
+    // Decode asynchronously
+    request.onload = function() {
 
-     /*
-     * Sound
-     */
-     this.audioCtx;
-     this.audioBuffer;
-     this.audioSource;
-     this.analyser;
-     this.frequencyData;
-     this.frequencyDataFull;
-     this.frequences;
-   }
+      self.processAudio(request.response);
 
-   SoundReact.prototype = {
-     init: function(){
-       this.cv = document.querySelector('canvas');
-       this.ctx = this.cv.getContext('2d');
-       this.ocv = document.createElement('canvas');
-       this.octx = this.ocv.getContext('2d');
-       this.onResize();
-       this.loadSound();
-     },
-     loadSound: function(url) {
-       var self = this;
-       var request = new XMLHttpRequest();
-       request.open('GET', audioFile, true);
-       request.responseType = 'arraybuffer';
+    }
+    request.send();
+  },
+  processAudio: function(response) {
+    var self = this;
 
-       // Decode asynchronously
-       request.onload = function() {
+    this.audioCtx = new AudioContext();
+    this.analyser = this.audioCtx.createAnalyser();
+    this.analyser.fftSize = 256;
+    this.frequencyData = new Uint8Array(this.analyser.frequencyBinCount);
+    this.frequences = new Float32Array(256);
 
-         self.processAudio(request.response);
+    this.audioCtx.decodeAudioData(response, function(buffer) {
 
-       }
-       request.send();
-     },
-     processAudio: function (response) {
-       var self = this;
+      // success callback
+      self.audioBuffer = buffer;
 
-       this.audioCtx = new AudioContext();
-       this.analyser = this.audioCtx.createAnalyser();
-       this.analyser.fftSize = 256;
-       this.frequencyData = new Uint8Array(this.analyser.frequencyBinCount);
-       this.frequences = new Float32Array(256);
+      // Create sound from buffer
+      self.audioSource = self.audioCtx.createBufferSource();
+      self.audioSource.buffer = self.audioBuffer;
 
-       this.audioCtx.decodeAudioData(response, function(buffer) {
+      // connect the audio source to context's output
+      self.audioSource.connect(self.analyser)
+      self.analyser.connect(self.audioCtx.destination)
 
-         // success callback
-         self.audioBuffer = buffer;
+      // play sound
+      self.audioSource.start();
 
-         // Create sound from buffer
-         self.audioSource = self.audioCtx.createBufferSource();
-         self.audioSource.buffer = self.audioBuffer;
+      self.render()
 
-         // connect the audio source to context's output
-         self.audioSource.connect( self.analyser )
-         self.analyser.connect( self.audioCtx.destination )
+    }, function() {
 
-         // play sound
-         self.audioSource.start();
+      // error callback
 
-         self.render()
+    });
+  },
+  render: function() {
 
-       }, function(){
+    //this.frame(0);
+    window.addEventListener('resize', this.onResize.bind(this));
+    rafId = requestAnimationFrame(this.frame.bind(this));
+  },
+  frame: function(ms) {
+    var self = this;
+    rafId = requestAnimationFrame(this.frame.bind(this))
+    DELTA_TIME = Date.now() - LAST_TIME;
+    LAST_TIME = Date.now();
 
-         // error callback
+    frameIndex++;
 
-       });
-     },
-     render: function () {
+    this.analyser.getByteFrequencyData(this.frequencyData);
 
-       this.frame(0);
-       window.addEventListener( 'resize', this.onResize.bind(this) );
-       rafId = requestAnimationFrame( this.frame.bind(this) );
-     },
-     frame: function ( ms ) {
-       var self = this;
-       rafId = requestAnimationFrame( this.frame.bind(this) )
-       DELTA_TIME = Date.now() - LAST_TIME;
-       LAST_TIME = Date.now();
-
-       frameIndex++;
-
-       this.analyser.getByteFrequencyData(this.frequencyData);
-       this.drawFrequency(this.ctx, true, ms)
-
-     },
-
-
-     drawFrequency: function(ctx, shouldClear, ms) {
-
-       // ctx.translate(0, TRANSLATESTEP);
-
-       var nbLines = W / LINELENGTH;
-
-       var cumul = 0;
-       var average = 0;
-       var lines = [];
-       var equalizerW = W / 2;
-       var equalizerH = H / 4;
-       var len = this.frequencyData.length;
+    // Draw 
+    this.ctx.clearRect(0,0,W,H);
+    if(frameIndex > 10) {
+      this.sun.draw(this.ctx);
+    }
+    this.canyon.draw(this.ctx, this.frequencyData, ms);
+    this.ctx.restore();
     
-         this.ctx.fillStyle = '#00112204';
-         this.ctx.fillRect(0, 0, W, H);
 
-         this.ctx.save();
-         this.ctx.translate(W/2, H/2);
-         this.ctx.lineWidth = 4;
+  },
+  onResize: function(evt) {
 
-         var zoomLevel = 1.02;
-         this.ctx.drawImage(
-          this.cv,
-          0, 0, W, H,
-          -W/2*zoomLevel, -H/2 * zoomLevel, W * zoomLevel, H * zoomLevel
-         );
+    this.cvS = [window.innerWidth, window.innerHeight]
 
-         this.ctx.globalCompositeOperation = 'lighter';
+    this.cv.width = W;
+    this.cv.height = H;
+    this.cv.style.width = W + 'px'
+    this.cv.style.height = H + 'px';
+    this.ocv.width = W;
+    this.ocv.height = H;
+    this.ocv.style.width = W; + 'px'
+    this.ocv.style.height = H; + 'px'
 
-         this.ctx.strokeStyle = 'hsl(' + ms / 300 + ', 50%, 50%)';
-         
-      if((frameIndex % 2) === 0) {
-        this.ctx.beginPath();
-        this.ctx.lineTo(- W/2,  - 40 );
-       for ( var i = 0; i < len; i += 2 ) {
-          var index = i < len / 2 ? i : len - 1 - i; // on parcours le tableau dans l'autre sens quand on atteint la moitié de len
-         // get the frequency according to current i
-         var percentIdx = i / nbLines;
-         var frequencyIdx = Math.floor(2048 * percentIdx);
+  },
+  render: function() {
 
-          this.frequences[index] += (this.frequencyData[index] - this.frequences[index]) * EASING;
+    this.frame();
+    window.addEventListener('resize', this.onResize.bind(this));
+    rafId = requestAnimationFrame(this.frame.bind(this));
+  }
+}
 
-          var v = 1 - this.frequences[index] / 256;
-         
-         this.ctx.lineTo(( i / len - .5 ) * equalizerW, v * equalizerH * 0.8);
+/*
+* Canvas visual objects
+*/
+
+// Frequency Line
+function FreqLine(props) {
+  this.w = props.w;
+  this.h = props.h;
+  this.lineWidth = props.lineWidth;
+  this.startOffset = props.startOffset;
+  this.easeFactor = props.easeFactor;
+}
+
+FreqLine.prototype = {
+  draw: function(ctx, audioData) {
+    ctx.lineWidth = this.lineWidth;
+    ctx.beginPath();
+    ctx.lineTo(-W / 2, this.startOffset);
+    for (var i = 0; i < audioData.length; i += 2) {
+      var index = i < audioData.length / 2 ? i : audioData.length - 1 - i; // on parcours le tableau dans l'autre sens quand on atteint la moitié de audioData.length
+      // get the frequency according to current i
+      frequences[index] += (audioData[index] - frequences[index]) * this.easeFactor;
+      var v = 1 - frequences[index] / 256;
+      ctx.lineTo((i / audioData.length - .5) * this.w, v * this.h * 0.8);
+    }
+    ctx.lineTo(W / 2, this.startOffset);
+    ctx.stroke();
+  }
+}
+
+// Canyon 
+function Canyon(props) {
+  this.x = props.x;
+  this.y = props.y;
+  this.octx = props.octx;
+  this.ocv = props.ocv;
+  this.zoom = props.zoom;
+  this.canyonLine = props.canyonLine;
+}
+
+Canyon.prototype = {
+  draw: function(ctx, audioData, ms) {
+
+    // Clip canyon
+    ctx.save();
+    ctx.fillStyle = '#0e0e0e';
+    ctx.translate(W / 2, H / 2);
+    this.canyonLine.draw(ctx, audioData);
+    ctx.lineTo(W / 2,H /2);
+    ctx.lineTo(-W/2,H/2);
+    ctx.closePath();
+    ctx.fill();
+    ctx.clip();
+    ctx.translate(-W/2, -H/2);
+
+    // reset canvas
+    this.octx.fillStyle = 'rgba(28,28,28,0.03)';
+    this.octx.fillRect(0, 0, W, H);
+
+    // begin draw lines
+    this.octx.save();
+    this.octx.translate(W / 2, H / 2);
+
+    // draw image 
+    this.octx.drawImage(
+      this.ocv,
+      0, 0, W, H, -W / 2 * this.zoom, -H / 2 * this.zoom, W * this.zoom, H * this.zoom
+    );
+    // >>>> draw image opti :
+    //this.octx.drawImage(this.cv, -W/2, lineBeginOffset, W, equalizerH, this.zoom, this.zoom, W * this.zoom, equalizerH * this.zoom);
+
+    this.octx.globalCompositeOperation = 'lighter';
+
+    // TODO: add in a function
+    // Animate light 
+    if (iterationCount < totalIterations) {
+      lightEasingValue = Math.easeOutQuad(iterationCount, 10, 50 - 10, totalIterations);
+      //iterationCount++;  
+    }
+
+    this.octx.strokeStyle = 'hsl(' + ms / 300 + ', ' + lightEasingValue + '%, ' + lightEasingValue + '%)';
+
+    if ((frameIndex % 2) === 0) {
+      this.canyonLine.draw(this.octx, audioData);
+    }
+
+    this.octx.globalCompositeOperation = 'source-over';
+
+    this.octx.restore();
+
+    ctx.drawImage(this.ocv, 0, 0);
+  }
+};
 
 
-       }
-        this.ctx.lineTo(W / 2,  - 40 );
-        this.ctx.stroke();
-      }
+// Sun rising at the center
+function Sun(props) {
+  this.x = props.x;
+  this.y = props.y;
+  this.r = props.r;
+  this.nbPoints = props.nbPoints;
+  this.points = [];
+}
 
-      this.ctx.globalCompositeOperation = 'source-over';
-
-       this.ctx.restore();
-
-     },
-
-
-     onResize: function ( evt ) {
-
-       this.cvS = [window.innerWidth, window.innerHeight]
-
-       this.cv.width = W;
-       this.cv.height = H;
-       this.cv.style.width = W + 'px'
-       this.cv.style.height = H + 'px';
-       this.ocv.width = W;
-       this.ocv.height = H;
-       this.ocv.style.width = W; + 'px'
-       this.ocv.style.height = H; + 'px'
-
-     },
-     render: function () {
-
-       this.frame();
-       window.addEventListener( 'resize', this.onResize.bind(this) );
-       rafId = requestAnimationFrame( this.frame.bind(this) );
-     }
-   };
+Sun.prototype = {
+  setPoints: function() {
+    step = 2 * Math.PI / this.nbPoints;
+    for (var i = this.nbPoints - 1; i >= 0; i--) {
+      this.points.push(
+        [
+          this.r * Math.cos(i * step),
+          this.r * Math.sin(i * step)
+        ]
+      );
+    }
+  },
+  draw: function(ctx) {
+    ctx.save();
 
 
-   soundReact = new SoundReact();
-   soundReact.init();
+    // On veut changer le this.y :
+    if (iterationCount < totalIterations) {
+      sunEasingValue = Math.easeOutQuad(iterationCount, this.y + this.r*1.5, this.y - (this.y + this.r*1.5), totalIterations);
+      iterationCount++;  
+    } 
+    if((iterationCount % 30) === 0) {
+        console.log(iterationCount/30); 
+    }
+    //}
+    ctx.translate(this.x, sunEasingValue);
+    //ctx.clearRect(0,0,W,H);
+    ctx.fillStyle = 'rgba(245, 245, 245, 1)';
+    ctx.beginPath();
+
+    for (var i = this.points.length - 1; i >= 0; i--) {
+      ctx.lineTo(this.points[i][0], this.points[i][1]);
+
+      //noise = ((this.points[i][0] + Math.cos(Math.random())) * 5 - noise) * 0.3;
+      //console.log(noise)
+      //ctx.lineTo(this.points[i][0], this.points[i][1]);
+    }
+    ctx.closePath();
+    ctx.fill();
+    ctx.restore();
+  }
+} 
+
+
+/**
+ *
+ * Init
+ *
+ */
+scene = new Scene();
+scene.init();
 
 
 /**
@@ -218,14 +358,26 @@
  *
  */
 
- // map a range of numbers to another range of numbers
- Number.prototype.map = function(in_min, in_max, out_min, out_max) {
-   return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
- }
+// map a range of numbers to another range of numbers
+Number.prototype.map = function(in_min, in_max, out_min, out_max) {
+  return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 
- function addLimited(arr, x, limit) {
-     arr.unshift(x);
-     if (arr.length > limit) {
-         arr.length = limit;
-     }
- }
+function addLimited(arr, x, limit) {
+  arr.unshift(x);
+  if (arr.length > limit) {
+    arr.length = limit;
+  }
+}
+
+// t: current iteration
+// b: begin value
+// c: change value
+// d: total iterations
+Math.easeOutQuad = function (t, b, c, d) {
+  t /= d;
+  return -c * t*(t-2) + b;
+};
+
+
+
